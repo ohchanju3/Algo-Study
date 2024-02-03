228640170db52cf967ba8e38ea8b5a3d5e8d4d98
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-02-04 01:32:06 +0900

    [Bronze V] Title: 구구단, Time: 160 ms, Memory: 16244 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        for(int i=1; i<10; i++){
+            bw.write(n+" * "+i+" = "+(n*i)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
