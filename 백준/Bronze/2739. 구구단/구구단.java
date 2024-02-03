86f83fa0d379d383c3101f38e425d1651911595e
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-02-04 01:57:13 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16528 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -16,6 +16,7 @@ public class Main {
         for(int i=1; i<10; i++){
             bw.write(n+" * "+i+" = "+(n*i)+"\n");
         }
+        // bw.flush는 남아있는 데이터를 모두 출력하는 것!
         bw.flush();
         bw.close();
     }
