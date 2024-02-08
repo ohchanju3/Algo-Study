454a37fd70f73c13e3489689d1dc1466fc5d972d
Sangjun Lee <lsjmc0224@dgu.edu>
2024-02-08 10:33:21 +0900

    [BOJ] / 수학은 체육과목 입니다 / bronze3 / https://www.acmicpc.net/problem/15894

---
+++ 이상준/3주차/3일차 [BOJ] 15894.py
@@ -0,0 +1,12 @@
+# 수학은 체육과목입니다. ( https://www.acmicpc.net/problem/15894 )
+# input 가장 아랫부분의 정사각형 개수 N
+N = int(input())
+
+# output 도형의 둘레
+output = 4*N
+print(output)
+
+'''
+알게된 것
+1. 이게 왜 브론즈 3?
+'''
