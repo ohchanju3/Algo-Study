6bfebf306dfc455dbe9b0eca2a858c8c5ef3a0a4
Ohchanju <ohchanju3@naver.com>
2024-02-18 19:00:01 +0900

    [Pro] / 옷가게 할인받기 / 입문 / https://school.programmers.co.kr/learn/courses/30/lessons/120818

--- 오찬주/4주차/[Pro]120908_문자열안의문자열.js
+++ 오찬주/4주차/[Pro]120908_문자열안의문자열.js
@@ -29,3 +29,19 @@ function solution(str1, str2) {
 // var str2 = "po";
 
 // console.log(solution(str1, str2));
+
+//다른 문제 풀이
+/*
+split() 함수
+
+string.split(separator, limit)
+
+
+*/
+
+function solution(str1, str2) {
+  //str2를 기준으로 str1을 분할
+  //만약 분할된게 없다? 즉,str1의 분할된 length가 1 초과다?
+  // str2가 str1에 없다!!는 뜻
+  return str1.split(str2).length > 1 ? 1 : 2;
+}
