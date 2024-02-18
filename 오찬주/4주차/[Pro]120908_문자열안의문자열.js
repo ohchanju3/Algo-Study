b4da7e8fce95d802474782f8de0f6eb7537444a3
Ohchanju <ohchanju3@naver.com>
2024-02-18 18:27:14 +0900

    [Pro] / 문자열 안의 문자열 / 입문 / https://school.programmers.co.kr/learn/courses/30/lessons/120908

---
+++ 오찬주/4주차/[Pro]120908_문자열안의문자열.js
@@ -0,0 +1,31 @@
+/*
+
+[문자열 안의 문자열]
+
+문제 설명
+문자열 str1, str2가 매개변수로 주어집니다. 
+str1 안에 str2가 있다면 1을, 없다면 2를 return하도록 
+solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ str1의 길이 ≤ 100
+1 ≤ str2의 길이 ≤ 100
+문자열은 알파벳 대문자, 소문자, 숫자로 구성되어 있습니다.
+
+*/
+
+//문제 풀이
+//includes 활용
+
+function solution(str1, str2) {
+  var answer = 2;
+  if (str1.includes(str2)) {
+    answer = 1;
+  }
+  return answer;
+}
+
+// var str1 = "poppy";
+// var str2 = "po";
+
+// console.log(solution(str1, str2));
