89b091252e514a6d97aaf6bd1448a1ed77326690
lsjmc0224 <lsjmc0224@dgu.edu>
2024-02-24 17:19:10 +0900

    [BOJ] / 부등호 / silver1 / https://www.acmicpc.net/problem/22529

--- 이상준/5주차/6일차 [BOJ] 2529.py
+++ 이상준/5주차/6일차 [BOJ] 2529.py
@@ -4,9 +4,57 @@ K = int(input())
 lst = list(map(str, input().split()))
 
 # output : 부등호 관계를 만족하는 최댓값과 최솟값
-'''
-작은수부터 가져와
-    1. 
+def min_num(lst, K):
+    num_lst = [i for i in range(10)]
+    temp_num_lst = num_lst
+    output = []
+    count = 0
+    for i in range(K):
+        if lst[i] == '>':
+            count = count + 1
+            if i == K-1:
+                for j in range(count, -1, -1):
+                    output.append(temp_num_lst[j])
+        else:
+            for j in range(count, 0, -1):
+                temp = temp_num_lst[j]
+                temp_num_lst.remove(temp)
+                output.append(temp)
+            
+            temp = temp_num_lst[0]
+            temp_num_lst.remove(temp)
+            output.append(temp)
+            count = 0
+            if i == K-1:
+                output.append(temp_num_lst[0])
+    
+    return ''.join(map(str, output))
 
-큰수부터 가져와
-'''
+def max_num(lst, K):
+    num_lst = [i for i in range(9, -1, -1)]
+    temp_num_lst = num_lst
+    output = []
+    count = 0
+    for i in range(K):
+        if lst[i] == '<':
+            count = count + 1
+            if i == K-1:
+                for j in range(count, -1, -1):
+                    output.append(temp_num_lst[j])
+        else:
+            for j in range(count, 0, -1):
+                temp = temp_num_lst[j]
+                temp_num_lst.remove(temp)
+                output.append(temp)
+            
+            temp = temp_num_lst[0]
+            temp_num_lst.remove(temp)
+            output.append(temp)
+            count = 0
+            if i == K-1:
+                output.append(temp_num_lst[0])
+    
+    return ''.join(map(str, output))
+
+print(max_num(lst,K))
+print(min_num(lst,K))
