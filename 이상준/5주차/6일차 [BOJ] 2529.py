fbaadc70fefd6bad6b8b8fdcfbd37877ad4bc85f
Sangjun Lee <lsjmc0224@dgu.edu>
2024-02-24 15:27:37 +0900

    임시저장

---
+++ 이상준/5주차/6일차 [BOJ] 2529.py
@@ -0,0 +1,12 @@
+# 부등호 ( https://www.acmicpc.net/problem/2529 )
+# input : 부등호의 개수와 부등호 문자열
+K = int(input())
+lst = list(map(str, input().split()))
+
+# output : 부등호 관계를 만족하는 최댓값과 최솟값
+'''
+작은수부터 가져와
+    1. 
+
+큰수부터 가져와
+'''
