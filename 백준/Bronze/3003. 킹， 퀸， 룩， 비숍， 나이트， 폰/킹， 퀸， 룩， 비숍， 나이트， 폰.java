cfcf1a5cc1159c65a4eed76ecbec6a2f13b53d5d
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-03-02 17:57:51 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 192 ms, Memory: 16840 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,32 +1,28 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int king = 1;
-		int queen = 1;
-		int rook = 2;
-		int bishop = 2;
-		int knight = 2;
-		int pawn = 8;
- 
-		king = king - in.nextInt();
-		queen = queen - in.nextInt();
-		rook = rook - in.nextInt();
-		bishop = bishop - in.nextInt();
-		knight = knight - in.nextInt();
-		pawn = pawn - in.nextInt();
- 
-		// 참고로 출력형식을 보면 각 변수들사이에 공백으로 구분 된한 줄로 출력해야한다.
-		// 즉, 개행(줄바꿈)이 발생하는 println을 쓰면 안된다.
-		System.out.print(king + " ");
-		System.out.print(queen + " ");
-		System.out.print(rook + " ");
-		System.out.print(bishop + " ");
-		System.out.print(knight + " ");
-		System.out.print(pawn);		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int king = 1;
+        int queen = 1;
+        int rook = 2;
+        int bishop = 2;
+        int knight = 2;
+        int pawn = 8;
+        
+        king = king-Integer.parseInt(st.nextToken());
+        queen = queen-Integer.parseInt(st.nextToken());;
+        rook = rook-Integer.parseInt(st.nextToken());;
+        bishop = bishop-Integer.parseInt(st.nextToken());;
+        knight = knight-Integer.parseInt(st.nextToken());;
+        pawn = pawn-Integer.parseInt(st.nextToken());;
+        
+        System.out.print(king+" "+queen+" "+rook+" "+bishop+" "+knight+" "+pawn);
+    }
 }
