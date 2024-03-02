60f71216360e8c480e9a0d65dbf1d0b9c1670dbe
lsjmc0224 <lsjmc0224@dgu.edu>
2024-03-03 00:01:12 +0900

    [BOJ] / 큐 / silver4 / https://www.acmicpc.net/problem/10845

--- 이상준/6주차/6일차 [BOJ] 10845.py
+++ 이상준/6주차/6일차 [BOJ] 10845.py
@@ -4,7 +4,6 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 que = []
-
 for _ in range(n):
     command = input().split()
     if command[0] == 'push': que.append(command[1])
@@ -13,9 +12,9 @@ for _ in range(n):
     elif command[0] == 'empty': print(0 if que else 1)
     elif command[0] == 'front': print(que[0] if que else -1)
     else: print(que[-1] if que else -1)
-    
+
 '''
-# 첫번째 시도 : 큐의 pop(0) 연산이 많이 들것 같아서 했으나.. 이게 왠걸 걍 pop쓰는게 시간이 단축되었음
+# 첫번째 시도 : 큐의 pop(0) 연산이 많이 들 것 같아서 했으나.. 이게 왠걸 걍 pop(0)쓰는게 시간이 단축되었음
 import sys
 from collections import deque
 input = sys.stdin.readline
