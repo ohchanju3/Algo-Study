9d2e7bf74af70fa82e4f2e48f84eeab864081e9c
Jieun Seo <127473767+DIDICHAA@users.noreply.github.com>
2024-03-06 20:15:11 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,6 +1,7 @@
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+arr = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 word = input()
-
-for i in croatia :
-    word = word.replace(i, '*')  # input 변수와 동일한 이름의 변수
-print(len(word))
+  
+for i in arr:
+    word = word.replace(i, '*') #해당하는 단어를 *로 바꿈
+        
+print(len(word)) #
