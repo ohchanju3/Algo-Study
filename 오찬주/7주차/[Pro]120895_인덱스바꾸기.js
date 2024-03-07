c2562a8cf73bbe2297d204c96fcb9e2b8ecc7e85
Ohchanju <ohchanju3@naver.com>
2024-03-07 23:13:25 +0900

    [Pro] / 인덱스 바꾸기 / 입문 / https://school.programmers.co.kr/learn/courses/30/lessons/120895

---
+++ 오찬주/7주차/[Pro]120895_인덱스바꾸기.js
@@ -0,0 +1,31 @@
+/*
+
+[인덱스 바꾸기]
+
+<문제 설명>
+문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, 
+my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 
+return 하도록 solution 함수를 완성해보세요.
+
+<제한사항>
+1 < my_string의 길이 < 100
+0 ≤ num1, num2 < my_string의 길이
+my_string은 소문자로 이루어져 있습니다.
+num1 ≠ num2
+
+*/
+
+/*
+
+[문제 풀이]
+구조 분해 할당을 이용해보자!!!!
+
+*/
+
+function solution(my_string, num1, num2) {
+  var answer = [...my_string];
+
+  [answer[num1], answer[num2]] = [answer[num2], answer[num1]];
+
+  return answer.join("");
+}
