b98ea27f504ceab696e388fe561b0f3551f3bc21
Hyeona01 <sha0209@dgu.ac.kr>
2024-04-07 22:11:46 +0900

    [BOJ] / 세로읽기 / 브론즈1 / https://www.acmicpc.net/problem/10798

---
+++ 설현아/10주차/10798 세로읽기/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    char[][] ch = new char[5][15];
+    int max = 0;
+
+    for (int i = 0; i < ch.length; i++) {
+      String str = sc.nextLine();
+      if (max < str.length()) {
+        max = str.length();
+      }
+      for (int j = 0; j < str.length(); j++) {
+        ch[i][j] = str.charAt(j);
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < max; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (ch[j][i] == '\0')
+          continue;
+        result.append(ch[j][i]);
+      }
+    }
+    System.out.println(result);
+
+  }
+  // 너무 어렵다..
+}
